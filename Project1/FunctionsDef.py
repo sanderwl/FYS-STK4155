7978d8118da152fc0f89fe951364ae6caa647643
sanderwl <31347023+sanderwl@users.noreply.github.com>
2020-09-20 00:55:23 +0200

    qqq
    qqq

--- Project1/FunctionsDef.py
+++ Project1/FunctionsDef.py
@@ -16,7 +16,7 @@ def MSE(y_data, y_pred):
     n = np.size(y_data)
     return (np.sum((y_data-y_pred)**2))/n
 
-def FrankePlot(x, y):
+def FrankePlot(x, y, plot):
     # Terms of the Franke function
     term1 = 0.75 * np.exp(-(0.25 * (9 * x - 2) ** 2) - 0.25 * ((9 * y - 2) ** 2))
     term2 = 0.75 * np.exp(-((9 * x + 1) ** 2) / 49.0 - 0.1 * (9 * y + 1))
@@ -24,18 +24,19 @@ def FrankePlot(x, y):
     term4 = -0.2 * np.exp(-(9 * x - 4) ** 2 - (9 * y - 7) ** 2)
     z = term1 + term2 + term3 + term4
 
-    # Plot the Franke function
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
-                           linewidth=0, antialiased=False)
-    # Labels
-    ax.set_zlim(-0.10, 1.40)
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-    # Color map grid
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-    plt.show()
+    if plot == True:
+        # Plot the Franke function
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+        # Labels
+        ax.set_zlim(-0.10, 1.40)
+        ax.zaxis.set_major_locator(LinearLocator(10))
+        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+        # Color map grid
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+        plt.show()
+
     return z
 
 def Scale(x):
@@ -44,25 +45,26 @@ def Scale(x):
     d = standardize.transform(x)
     return d
 
-def StdPolyOLS(X,z):
+def StdPolyOLS(X_scale,z):
 
-    # Data split and scaling
-    X_train, X_test, Y_train, Y_test = train_test_split(X, z, test_size=0.2)
-    X_train_scale = Scale(X_train)
-    X_test_scale = Scale(X_test)
+    # Data split
+    X_train_scale, X_test_scale, Y_train, Y_test = train_test_split(X_scale, z, test_size=0.2)
+
 
     # Ordinary least squares using matrix inversion
     betas = np.linalg.pinv(X_train_scale.T @ X_train_scale) @ X_train_scale.T @ Y_train
-    print("Beta coefficients for ordinary least squares: ", betas)
 
     # Predict the response
-    Y_train_pred = X_train @ betas
-    Y_test_pred = X_test @ betas
+    Y_train_pred = X_train_scale @ betas
+    Y_test_pred = X_test_scale @ betas
 
     # Calculate and print the train and test MSE
     print("Training MSE for ordinary least squares: ", MSE(Y_train, Y_train_pred), ".")
     print("Test MSE for ordinary least squares: ", MSE(Y_test, Y_test_pred), ".")
-    return Y_train_pred, Y_test_pred, betas
+
+    z_pred = X_scale @ betas
+
+    return z_pred, Y_train_pred, Y_test_pred, betas
 
 def designMatrix(x,y,poly):
     p = np.arange(1,poly+1,1)
@@ -72,9 +74,15 @@ def designMatrix(x,y,poly):
 
     return designMatrix
 
-def mean_confidence_interval(data, confidence=0.95):
-    a = 1.0 * np.array(data)
-    n = len(a)
-    m, se = np.mean(a), scipy.stats.sem(a)
-    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
-    return m, m-h, m+h
+def betaConfidenceInterval(true, predict, n, degreeFreedom, X, betas):
+
+    sigma2 = (1 / (n - (degreeFreedom - 1))) * (sum((true - np.mean(true)) ** 2))
+    # Betas varians:
+    varBeta = np.linalg.pinv((X.T @ X)) * sigma2
+
+    # estimert standardavvik pr beta.
+    # Må forklares i rapport hvorfor man velger å gjøre det slik og ikke på andre måter.
+    betaCoeff = (np.sqrt(np.diag(varBeta)))
+    # Intervall betacoefisienter:
+    beta_confInt = np.c_[betas - betaCoeff, betas + betaCoeff]
+    return beta_confInt
--- Project1/Project1PF.py
+++ Project1/Project1PF.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import sklearn.linear_model as skl
-from FunctionsDef import FrankePlot, MSE, R2,StdPolyOLS,designMatrix,mean_confidence_interval
+from FunctionsDef import FrankePlot, MSE, R2,StdPolyOLS,designMatrix,betaConfidenceInterval,Scale
 import scipy.stats
 
 # Create data
@@ -17,13 +17,22 @@ y = np.arange(0, 1, 1/n)
 x, y = np.meshgrid(x,y)
 
 # Setting up the Franke function
-z = FrankePlot(x, y).ravel()
+z = FrankePlot(x, y, plot = False).ravel()
 
 # Setting up the polynomial design matrix
 designMatrix = designMatrix(x,y,poly)
 
+# Scaling design matrix by subtracting mean and dividing my variance
+designMatrix_scale = Scale(designMatrix)
+
 # Performing ordinary least squares
-Y_train_pred, Y_test_pred, betas = StdPolyOLS(designMatrix,z)
+z_pred, Y_train_pred, Y_test_pred, betas = StdPolyOLS(designMatrix_scale,z)
+#print(betas)
+
 # Confidence interval
-c1,c,c2 = mean_confidence_interval(betas)
-print(c1,c,c2)
+beta_confInt = betaConfidenceInterval(z, z_pred, n, 1, designMatrix_scale, betas)
+
+fig = plt.figure()
+plt.errorbar(np.arange(0,len(betas),1),betas,yerr=beta_confInt[:,0])
+plt.show()
+

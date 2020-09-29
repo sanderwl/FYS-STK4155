18f079a6757ed7796a12288f6b6ad9ddee74b7fc
sanderwl <31347023+sanderwl@users.noreply.github.com>
2020-09-30 00:05:09 +0200

    yessssssss
    endelig

--- Project1/FunctionsDef.py
+++ Project1/FunctionsDef.py
@@ -8,6 +8,7 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import sklearn.linear_model as skl
 import scipy.stats
 from numpy import array
+from mlxtend.evaluate import bias_variance_decomp
 
 def inputsss():
     observations = input("Enter number of observations n (integer): ")
@@ -24,10 +25,12 @@ def inputsss():
     noiseAR = input("Add random noise? (yes/no)")
     if noiseAR == "yes":
         print("Noise will be added")
+        noiseLVL = input("With what multiplicative effect? (float)")
         noiseInp = True
     else:
         print("Noise will NOT be added")
         noiseInp = False
+        noiseLVL = 0
     figurePrint = input("Plot figures? (yes/no)")
     if figurePrint == "yes":
         print("Figures will be plotted")
@@ -36,24 +39,41 @@ def inputsss():
         print("Figures will NOT be plotted")
         figureInp = False
     part = input("Which sub-exericse will you run(a,b,c,d,e,f/all)?")
-    return observations, degree, scaleInp, figureInp, part, noiseInp
+    testlevel = input("What is the test size? (between 0 and 1, but typically around 0.25)")
+    return observations, degree, scaleInp, figureInp, part, noiseInp, noiseLVL,testlevel
 
 
 def R2(f, y):
     return 1 - (np.sum((y - f) ** 2) / np.sum((f - np.mean(f)) ** 2))
 
 def MSE(y_data, y_pred):
-    n = np.size(y_data)
+    n = np.size(y_pred)
     return (np.sum((y_data-y_pred)**2))/n
 
-def FrankePlot(x, y, plot):
+def calc_Variance(y_real, y_pred,n):
+    var = np.var(y_pred)
+    return var
+
+def calc_bias(y_real, y_pred,n):
+    bias = np.mean((np.mean(y_pred) - y_real)** 2) - calc_Variance(y_real,y_pred,n)
+    return bias
+
+def FrankeFunc(x, y):
     # Terms of the Franke function
     term1 = 0.75 * np.exp(-(0.25 * (9 * x - 2) ** 2) - 0.25 * ((9 * y - 2) ** 2))
     term2 = 0.75 * np.exp(-((9 * x + 1) ** 2) / 49.0 - 0.1 * (9 * y + 1))
     term3 = 0.5 * np.exp(-(9 * x - 7) ** 2 / 4.0 - 0.25 * ((9 * y - 3) ** 2))
     term4 = -0.2 * np.exp(-(9 * x - 4) ** 2 - (9 * y - 7) ** 2)
     z = term1 + term2 + term3 + term4
+    return z
 
+def FrankPlot(x,y,plot):
+    # Terms of the Franke function
+    term1 = 0.75 * np.exp(-(0.25 * (9 * x - 2) ** 2) - 0.25 * ((9 * y - 2) ** 2))
+    term2 = 0.75 * np.exp(-((9 * x + 1) ** 2) / 49.0 - 0.1 * (9 * y + 1))
+    term3 = 0.5 * np.exp(-(9 * x - 7) ** 2 / 4.0 - 0.25 * ((9 * y - 3) ** 2))
+    term4 = -0.2 * np.exp(-(9 * x - 4) ** 2 - (9 * y - 7) ** 2)
+    z = term1 + term2 + term3 + term4
     if plot == True:
         # Plot the Franke function
         fig = plt.figure()
@@ -67,18 +87,22 @@ def FrankePlot(x, y, plot):
         fig.colorbar(surf, shrink=0.5, aspect=5)
         plt.show()
 
-    return z
-
 def Scale(X_train, X_test, scalee):
     if scalee == True:
         scaler = StandardScaler()
         X_train_scale = scaler.fit_transform(X_train)
         X_test_scale = scaler.transform(X_test)
-        print("Data is scaled.")
     else:
         X_train_scale = X_train
         X_test_scale = X_test
     return X_train_scale, X_test_scale
+
+def Scale2(designMatrix, scalee):
+    if scalee == True:
+        designMatrix_scale = (designMatrix-np.mean(designMatrix))/ np.std(designMatrix)
+    else:
+        designMatrix_scale = designMatrix
+    return designMatrix_scale
 
 def StdPolyOLS(X_train, X_test, Y_train, Y_test):
 
@@ -88,6 +112,8 @@ def StdPolyOLS(X_train, X_test, Y_train, Y_test):
     # Predict the response
     Y_train_pred = X_train @ betas
     Y_test_pred = X_test @ betas
+
+    print(R2(Y_test,Y_test_pred))
 
     return Y_train_pred, Y_test_pred, betas
 
@@ -105,17 +131,54 @@ def designMatrixFunc(x, y, poly):
         params = PolynomialFeatures(p).get_params(deep=True)
     return designMatrix, params
 
-def designMatrixFunc2(x, y, poly):
+def designMatrixFunc2(x, y, poly, noiseLVL):
 
     preds = np.c_[x.ravel(), y.ravel()]
-    designMatrix = PolynomialFeatures(poly).fit_transform(preds)
+    designMatrix = PolynomialFeatures(degree=poly, include_bias=False).fit_transform(preds)
+    data = np.column_stack((np.ones((len(designMatrix),1)), designMatrix + float(noiseLVL)*np.random.randn(len(x),len(designMatrix[0]))))
 
-    return designMatrix
+    return data
 
 def sciKitSplit(designMatrix,z,testsize,shufflezzz):
 
-    X_train, X_test, Y_train, Y_test = train_test_split(designMatrix, z, test_size=testsize, random_state=13 , shuffle=shufflezzz)
+    X_train, X_test, Y_train, Y_test = train_test_split(designMatrix, z, test_size=testsize, random_state=13, shuffle=shufflezzz)
     return X_train, X_test, Y_train, Y_test
+
+def realSplit(designMatrix,z,testsize,i,rn, shufflezzz):
+
+    n = len(designMatrix)
+
+    designMatrix2 = np.array(designMatrix)
+    z2 = np.array(z)
+    Xtest = np.zeros((int(testsize*n), len(designMatrix2[0])))
+    Ytest = np.zeros(int(testsize*n))
+
+    if i == 0:
+        randomNumber = np.random.choice(n, size = round(testsize*n), replace = False)
+    else:
+        randomNumber = rn
+    for i in range(len(randomNumber)):
+        Xtest[i] = designMatrix2[randomNumber[i], :]
+        Ytest[i] = z2[randomNumber[i]]
+
+    Xtrain = np.delete(designMatrix2, randomNumber, 0)
+    Ytrain = np.delete(z2, randomNumber)
+
+    if i == 0:
+        data = np.column_stack((Xtrain, Ytrain))
+        np.random.shuffle(data)
+        randomNumber2 = np.random.randint(len(data[0]),size=1)
+        Ytrain = data[:,int(randomNumber2)]
+        Xtrain = np.delete(data,int(randomNumber2),1)
+        '''''''''''
+    else:
+        np.random.shuffle(Xtrain)
+        np.random.shuffle(Ytrain)
+'''''
+    return Xtrain, Xtest, Ytrain, Ytest, randomNumber
+
+
+
 
 def betaConfidenceInterval(true, n, degreeFreedom, X, betas, plot):
 
@@ -169,3 +232,42 @@ def CV(XX,z,n,k):
         n = n-1
 
     return Xtrain, Xtest, Ytrain, Ytest
+
+def MSEplot(MSE_train_poly,MSE_test_poly,poly):
+    xxx = np.arange(0, poly, 1)
+    plt.plot(xxx, MSE_train_poly, label="Train MSE", linewidth=2)
+    plt.plot(xxx, MSE_test_poly, label="Test MSE", linewidth=2)
+    plt.xticks(xxx)
+    MSE_min_test = MSE_test_poly.argmin()
+    plt.plot(xxx[MSE_min_test], MSE_test_poly[MSE_min_test], 'o', markersize=10, label="Lowest test MSE")
+    plt.suptitle('Training and test MSE as a function of polynomial degree (complexity)', fontsize=25,
+                 fontweight="bold")
+    plt.ylabel('MSE', fontsize=20)
+    plt.xlabel('Polynomial degree (complexity)', fontsize=20)
+    plt.legend(loc="upper right", prop={'size': 20})
+    plt.show()
+
+def BVPlot(bias,variance,mse,poly):
+    xxx = np.arange(0, poly, 1)
+    plt.plot(xxx, bias, label="Bias", linewidth=2)
+    plt.plot(xxx, variance, label="Variance", linewidth=2)
+    plt.xticks(xxx)
+    #bias_min = bias.argmin()
+    #plt.plot(xxx[bias_min], bias[bias_min], 'o', markersize=10, label="Lowest test bias")
+    plt.suptitle('Test bias and variance as a function of polynomial degree', fontsize=25,
+                 fontweight="bold")
+    plt.ylabel('Value', fontsize=20)
+    plt.xlabel('Polynomial degree (complexity)', fontsize=20)
+    plt.legend(loc="upper right", prop={'size': 20})
+    plt.show()
+
+    plt.plot(xxx,bias**2 + variance, label="Bias squared + variance", linewidth=2)
+    plt.plot(xxx,mse, label="MSE", linewidth=2)
+    plt.plot(xxx,bias**2 + variance - mse, label="Difference", linewidth=2)
+    plt.xticks(xxx)
+    plt.suptitle('MSE vs. bias squared + variance as a function of polynomial degree', fontsize=25,
+                 fontweight="bold")
+    plt.ylabel('Value', fontsize=20)
+    plt.xlabel('Polynomial degree (complexity)', fontsize=20)
+    plt.legend(loc="upper right", prop={'size': 20})
+    plt.show()
--- Project1/Project1PF.py
+++ Project1/Project1PF.py
@@ -7,11 +7,13 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import sklearn.linear_model as skl
-from FunctionsDef import inputsss, FrankePlot, MSE, R2,StdPolyOLS,designMatrixFunc, designMatrixFunc2,betaConfidenceInterval,Scale, sciKitSplit
+from FunctionsDef import inputsss, FrankeFunc, MSE, R2,StdPolyOLS,designMatrixFunc, designMatrixFunc2,\
+    betaConfidenceInterval,Scale, sciKitSplit, MSEplot, calc_Variance, calc_bias, BVPlot, Scale2, realSplit,FrankPlot
 import scipy.stats
+from mlxtend.evaluate import bias_variance_decomp
 
 # Input function
-observations, degree, scaleInp, figureInp, part, noiseInp = inputsss()
+observations, degree, scaleInp, figureInp, part, noiseInp, noiseLVL,testsizesss = inputsss()
 
 # Exercise 1a)
 
@@ -20,24 +22,23 @@ n = int(observations)
 poly = int(degree)
 x = np.arange(0, 1, 1 / n)
 y = np.arange(0, 1, 1 / n)
-x, y = np.meshgrid(x, y)
+xx, yy = np.meshgrid(x, y)
 
 # Setting up the Franke function
-if noiseInp == True:
-    z = FrankePlot(x, y, plot=figureInp).ravel() + 0.5 * np.random.randn(n ** 2)
-else:
-    z = FrankePlot(x, y, plot=figureInp).ravel()
+
+z = FrankeFunc(x, y,) + float(noiseLVL) * np.random.randn(n)
+FrankPlot(xx, yy, plot=figureInp)
+
 
 # Setting up the polynomial design matrix
-#designMatrix, params = designMatrixFunc(x, y, poly)
-designMatrix = designMatrixFunc2(x,y,poly)
+designMatrix = designMatrixFunc2(x,y,poly,noiseLVL)
 
 # Splitting data into training and test sets
-testSize = 0.2
-X_train, X_test, Y_train, Y_test = sciKitSplit(designMatrix, z, testSize, True)
+testSize = float(testsizesss)
+#X_train, X_test, Y_train, Y_test = sciKitSplit(designMatrix, z, testSize, True)
 
 #Scale data
-X_train_scale, X_test_scale = Scale(X_train, X_test, scalee = scaleInp)
+#X_train_scale, X_test_scale = Scale(X_train, X_test, scalee = scaleInp)
 
 if (str(part) == "a" or str(part) == "all"):
 
@@ -59,25 +60,26 @@ elif (str(part) == "b" or str(part) == "all"):
     # Exercise 1b)
     MSE_train_poly = np.zeros(poly)
     MSE_test_poly = np.zeros(poly)
+    bias = np.zeros(poly)
+    variance = np.zeros(poly)
     MSE_z = np.zeros(poly)
+    rn = 0
     for i in range(poly):
-        designMatrix_poly = designMatrixFunc2(x, y, i+1)
-        X_train_poly, X_test_poly, Y_train_poly, Y_test_poly = sciKitSplit(designMatrix_poly, z, testSize, True)
-        np.random.shuffle(X_train_poly)
-        np.random.shuffle(Y_train_poly)
-        X_train_scale_poly, X_test_scale_poly = Scale(X_train_poly, X_test_poly, scalee=scaleInp)
+        designMatrix_p = designMatrixFunc2(x, y, i+1, noiseLVL)
+        designMatrix_poly = Scale2(designMatrix_p,scalee=scaleInp)
+        X_train_scale_poly, X_test_scale_poly, Y_train_poly, Y_test_poly, randomNumber = realSplit(designMatrix_poly,z,testSize,i,rn,True)
+        rn = randomNumber
         Y_train_pred_poly, Y_test_pred_poly, betas_poly = StdPolyOLS(X_train_scale_poly, X_test_scale_poly, Y_train_poly, Y_test_poly)
+        bias[i] = calc_bias(Y_test_poly, Y_test_pred_poly, n)
+        variance[i] = calc_Variance(Y_test_poly, Y_test_pred_poly, n)
         MSE_train_poly[i] = MSE(Y_train_poly, Y_train_pred_poly)
         MSE_test_poly[i] = MSE(Y_test_poly, Y_test_pred_poly)
 
     if figureInp == True:
-        plt.plot(np.arange(0, poly, 1), MSE_train_poly, label ="Train MSE")
-        plt.plot(np.arange(0, poly, 1), MSE_test_poly, label ="Test MSE")
-        plt.suptitle('Training and test MSE as a function of polynomial degree (complexity)', fontsize=25)
-        plt.ylabel('MSE', fontsize=20)
-        plt.xlabel('Polynomial degree (complexity)', fontsize=20)
-        plt.legend(loc="upper right")
-        plt.show()
+        MSEplot(MSE_train_poly, MSE_test_poly, poly)
+        BVPlot(bias, variance, MSE_test_poly, poly)
+
+    print(MSE_test_poly - bias**2 - variance)
 
 elif (str(part) == "c" or str(part) == "all"):
     cvin = input("Number of folds in CV? (integer\False)")
